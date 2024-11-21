dfd7d6a5562dfbb887d3872f4f252caf488ee50b
patricio <patoare@hotmail.com>
2024-11-21 18:25:31 +0100

    Solution Lab1 Day4

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,29 +1,61 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
+function maxOfTwoNumbers(num1, num2) {
+    if (num1 > num2) {
+return num1
+    } else {
+        return num2
+    }
+}
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
-
-
-
+function findLongestWord(words) {
+    if (words.length === 0){
+        return null;
+      }
+      let longestWord = words[0];
+        
+      for (let i=1; i < words.length; i++) {
+        
+        if (words[i].length > longestWord.length) {
+          longestWord = words[i];
+        } 
+        }
+        return longestWord;
+}
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
-
+function sumNumbers(numbers) {
+    if (numbers.length === 0) {
+        return 0;
+      }
+  
+    let sum = 0;
+  for (let i=0; i < numbers.length; i++) {
+sum += numbers[i]
+  }
+  return sum
+    
+}
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    if (numbers2.length === 0) {
+        return 0;
+      }
+    let sum1 = 0;
+    for (let i=0; i < numbers2.length;  i++) {
+        sum1 += numbers2[i];
+    }
+    return sum1 / numbers2.length;   
+}
+console.log(averageNumbers(numbers2));
 
 
 
@@ -31,4 +63,19 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words2, wordToSearch) {
+    if (words2.length === 0) {
+        return null;
+      }
+ 
+      
+for (let i=0; i < words2.length; i++) {
+        if (words2[i] === wordToSearch) {
+return true;
+        } 
+    }
+ return false;
+        
+    }
+
+console.log(doesWordExist(words2))
